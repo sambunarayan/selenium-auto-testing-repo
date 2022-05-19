5d248f76f858547730e65874f732343d95801772
sambunarayan <soyou30257@gmail.com>
2022-05-19 23:32:38 +0900

    Selenium IEDriver

--- src/test/java/jeus/blog/auto/SampleTest.java
+++ src/test/java/jeus/blog/auto/SampleTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -27,13 +29,40 @@ public class SampleTest {
     @Test
     public void showPage() {
         //指定したURLに遷移する
-        webDriver.get("https://saikeblog.com");
+        webDriver.get("http://localhost:8080/app/v1/");
 
         // 最大5秒間、ページが完全に読み込まれるまで待つ
         webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 
+//        webDriver.findElement("");
+
         // 検証
 //        assertThat(webDriver.getTitle(), is("さいけの技術ブログ"));
+    }
+
+    @Test
+    public void testIE() {
+        System.setProperty("webdriver.ie.driver", "D:/Git/selenium-auto-testing-repo/selenium/iedriver/IEDriverServer.exe");
+
+        // Instantiate a IEDriver class.
+        WebDriver driver = new InternetExplorerDriver();
+        driver.get("http://www.google.com");
+
+//        driver.manage().window().maximize();
+
+        //Deleting all the cookies
+//        driver.manage().deleteAllCookies();
+
+        //launching the specified URL
+//        driver.get("https://www.google.com/");
+
+        //Locating the elements using name locator for the text box
+        driver.findElement(By.name("q")).sendKeys("BrowserStack");
+
+        //name locator for google search button
+        WebElement searchIcon = driver.findElement(By.name("btnK"));
+        searchIcon.click();
+
     }
 
     @Test
