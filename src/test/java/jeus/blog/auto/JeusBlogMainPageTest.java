73990cf614ea42b85965cb56719c99749bdbb5f6
sambunarayan <soyou30257@gmail.com>
2022-05-29 20:37:07 +0900

    Delete unnecessary import

--- src/test/java/jeus/blog/auto/JeusBlogMainPageTest.java
+++ src/test/java/jeus/blog/auto/JeusBlogMainPageTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JeusBlogMainPageTest {
